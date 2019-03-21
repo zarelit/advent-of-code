747835662589f92e14698b2496046be591077d7d
David Costa <david@zarel.net>
2019-03-21 12:36:03 +0100

    Move Challenge type in its own module
    In order to reuse the datatype in other modules without circular imports

--- src/Challenge.hs
+++ src/Challenge.hs
@@ -1,35 +1,9 @@
 module Challenge(
   Issue,
   Challenge(Challenge),
-  challenges,
-  print
 ) where
-
-import qualified Text.Printf as T
-import qualified Data.Map as Map
-import Data.Map ((!))
-import Prelude hiding (print)
 
 -- Issue is the unique identifier for a day of the AoC
 -- e.g. 2015, 22, B means Day 22, part B of the 2015 AoC
 type Issue = (Int, Int, String)
 data Challenge = Challenge (String -> Integer)
-
-inputPath :: Issue -> FilePath
-inputPath (year, day, _) = T.printf "data/%04d_%02d_input" year day
-
-outputPath :: Issue -> FilePath
-outputPath (year, day, part) = T.printf "data/%04d_%02d_%s_solved" year day part
-
--- All the implemented challenges
-challenges :: Map.Map Issue Challenge
-challenges = Map.fromList []
-
-run :: Issue -> String -> Integer
-run x = exercise where
-  Challenge exercise = challenges ! x
-
-print :: Issue -> IO ()
-print x = do
-  input <- readFile $ inputPath x
-  putStrLn $ show (run x input)
---
+++ src/ChallengeLauncher.hs
@@ -0,0 +1,31 @@
+module ChallengeLauncher(
+  challenges,
+  print
+) where
+
+import qualified Text.Printf as T
+import qualified Data.Map as Map
+import Data.Map ((!))
+import Prelude hiding (print)
+import Challenge
+
+import qualified AoC2015
+
+inputPath :: Issue -> FilePath
+inputPath (year, day, _) = T.printf "data/%04d_%02d_input" year day
+
+outputPath :: Issue -> FilePath
+outputPath (year, day, part) = T.printf "data/%04d_%02d_%s_solved" year day part
+
+-- All the implemented challenges
+challenges :: Map.Map Issue Challenge
+challenges = Map.fromList []
+
+run :: Issue -> String -> Integer
+run x = exercise where
+  Challenge exercise = challenges ! x
+
+print :: Issue -> IO ()
+print x = do
+  input <- readFile $ inputPath x
+  putStrLn $ show (run x input)
