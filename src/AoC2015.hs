9b081f154b8aa5ef1a2dec47e6e264ae929fded2
David Costa <david@zarel.net>
2019-04-07 19:49:47 +0200

    AoC 2015 Day 03

--- src/AoC2015.hs
+++ src/AoC2015.hs
@@ -3,7 +3,8 @@ module AoC2015(
 ) where
 
 import Data.Maybe (fromJust)
-import Data.List (elemIndex, sort)
+import Data.List (elemIndex, sort, nub)
+import Data.Char (isSpace)
 import Challenge
 
 year = 2015
@@ -13,7 +14,9 @@ challenges = [
   ((year, 1, "A"), Challenge santaFloor),
   ((year, 1, "B"), Challenge santaBasementIdx),
   ((year, 2, "A"), Challenge wrappingArea),
-  ((year, 2, "B"), Challenge ribbonLength)
+  ((year, 2, "B"), Challenge ribbonLength),
+  ((year, 3, "A"), Challenge uniqueVisitedHouses),
+  ((year, 3, "B"), Challenge uniqueVisitedHouses')
   ]
 
 {-
@@ -76,11 +79,11 @@ basement?
 -}
 
 -- trace returns the list of floors Santa moves to when given some instructions
-trace :: String -> [Int]
-trace instructions = scanl (+) 0 (map move instructions)
+traceMoves :: String -> [Int]
+traceMoves instructions = scanl (+) 0 (map move instructions)
 
 santaBasementIdx :: String -> Integer
-santaBasementIdx instructions = toInteger.fromJust $ elemIndex (-1) $ trace instructions
+santaBasementIdx instructions = toInteger.fromJust $ elemIndex (-1) $ traceMoves instructions
 
 {-
 --- Day 2: I Was Told There Would Be No Math ---
@@ -165,3 +168,97 @@ wrapRibbon (l, w, h) = minimum [2*l + 2*w, 2*l + 2*h, 2*w + 2*h]
 
 bowRibbon :: Present -> Integer
 bowRibbon (l, w, h) = l * w * h
+
+{-
+--- Day 3: Perfectly Spherical Houses in a Vacuum ---
+
+Santa is delivering presents to an infinite two-dimensional grid of houses.
+
+He begins by delivering a present to the house at his starting location, and
+then an elf at the North Pole calls him via radio and tells him where to move
+next. Moves are always exactly one house to the north (^), south (v), east (>),
+or west (<). After each move, he delivers another present to the house at his
+new location.
+
+However, the elf back at the north pole has had a little too much eggnog, and so
+his directions are a little off, and Santa ends up visiting some houses more
+than once. How many houses receive at least one present?
+
+For example:
+
+    > delivers presents to 2 houses: one at the starting location, and one to
+    > the east. ^>v< delivers presents to 4 houses in a square, including twice
+    > to the house at his starting/ending location. ^v^v^v^v^v delivers a bunch
+    > of presents to some very lucky children at only 2 houses.
+-}
+
+-- Position on a 2D grid, does not determine where origin is: (X, Y)
+newtype GridPosition = GridPos (Int, Int) deriving(Eq)
+
+-- Moves on a 2D grid: (N, E)
+newtype GridMove = GridMov (Int, Int)
+
+squareOrigin :: GridPosition
+squareOrigin = GridPos (0,0)
+
+asciiSquareMove :: Char -> GridMove
+asciiSquareMove x = case x of
+  'v' -> GridMov (0, -1)
+  '^' -> GridMov (0, 1)
+  '>' -> GridMov (1, 0)
+  '<' -> GridMov (-1, 0)
+  _ -> GridMov (0, 0)
+
+moveSquare :: GridPosition -> GridMove -> GridPosition
+moveSquare (GridPos (ox, oy)) (GridMov (x,y))  = GridPos (ox+x, oy+y)
+
+mirrorMove :: GridMove -> GridMove
+mirrorMove (GridMov (x,y)) = GridMov (-x, -y)
+
+mirrorPos :: GridPosition -> GridPosition
+mirrorPos (GridPos (x,y)) = GridPos (-x, -y)
+
+-- apply the move but inverting North and East
+moveSquareSpecular :: GridPosition -> GridMove -> GridPosition
+moveSquareSpecular p m = moveSquare p (mirrorMove m)
+
+traceSquarePath :: [GridMove] -> [GridPosition]
+traceSquarePath moves = scanl moveSquare squareOrigin moves
+
+parseDay03 :: String -> [GridMove]
+parseDay03 = map asciiSquareMove
+
+uniqueVisitedHouses :: String -> Integer
+uniqueVisitedHouses = toInteger.length.nub.traceSquarePath.parseDay03
+
+{- --- Part Two ---
+
+The next year, to speed up the process, Santa creates a robot version of
+himself, Robo-Santa, to deliver presents with him.
+
+Santa and Robo-Santa start at the same location (delivering two presents to the
+same starting house), then take turns moving based on instructions from the elf,
+who is eggnoggedly reading from the same script as the previous year.
+
+This year, how many houses receive at least one present?
+
+For example:
+
+    ^v delivers presents to 3 houses, because Santa goes north, and then
+    Robo-Santa goes south. ^>v< now delivers presents to 3 houses, and Santa and
+    Robo-Santa end up back where they started. ^v^v^v^v^v now delivers presents
+    to 11 houses, with Santa going one direction and Robo-Santa going the other.
+-}
+movesCouples :: [GridMove] -> [(GridMove,GridMove)]
+movesCouples (a:b:xs) = (a, b) : movesCouples xs
+movesCouples [] = []
+movesCouples _ = error "Odd number of moves"
+
+uniqueVisitedHouses' input = toInteger.length.nub $ allVisited
+  where
+    allVisited = visited santa ++ visited robot
+    visited xs = traceSquarePath xs
+    couples = movesCouples.parseDay03.strip $ input
+    santa = map fst couples
+    robot = map snd couples
+    strip = takeWhile (\x->not (isSpace x))
