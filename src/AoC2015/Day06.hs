2b463412c810f5a437c2c64d170578db014b6ddd
David Costa <david@zarel.net>
2019-07-07 16:51:51 +0200

    Split parsing and type definitions  Day 06 / 2015

--- src/AoC2015/Day06.hs
+++ src/AoC2015/Day06.hs
@@ -5,14 +5,12 @@ module AoC2015.Day06-- (partA, partB)
 where
 import Challenge
 
-import Text.ParserCombinators.ReadP
-import Data.Char (isDigit, isAlpha)
-import Control.Applicative ((<|>))
-import Data.List (find)
-import Data.Maybe (mapMaybe)
 import Data.Matrix (Matrix, matrix, elementwiseUnsafe, fromList)
 import Control.DeepSeq(NFData, rnf, force)
 import Data.Foldable (foldl')
+
+import AoC2015.Day06.Types
+import AoC2015.Day06.Parse(parse)
 
 {-
 --- Day 6: Probably a Fire Hazard ---
@@ -44,63 +42,6 @@ For example:
 
 After following the instructions, how many lights are lit?
 -}
-
-data Instruction = Instruction Op Range deriving Show
-data Op = Off | On | Toggle | Nop deriving Show
-data Range = Range Start End deriving Show
-type Start = Point
-type End = Start
-data Point = Point Int Int deriving Show
-
----- Parsing of the instructions
-
-instruction :: ReadP Instruction
-instruction = do
-  op <- operation
-  char ' '
-  r <- range
-  return $ Instruction op r
-
-operation :: ReadP Op
-operation = let
-  -- parse any string with spaces and letters and let opLookup be the source
-  -- of truth for the String reprentation of Op
-  opStr = many1 (satisfy isAlpha <|> satisfy (== ' '))
-  in do
-    op <- opStr
-    maybe pfail return $ opLookup op
-
-opLookup :: String -> Maybe Op
-opLookup "turn on" = Just On
-opLookup "turn off" = Just Off
-opLookup "toggle" = Just Toggle
-opLookup _ = Nothing
-
-range :: ReadP Range
-range = do
-  a <- point
-  string " through "
-  b <- point
-  return $ Range a b
-
-point :: ReadP Point
-point = let
-  number = fmap read (many1 $ satisfy isDigit)
-  in do
-    a <- number
-    satisfy (== ',')
-    b <- number
-    return $ Point a b
-
-parseInstruction :: String -> Maybe Instruction
-parseInstruction s = let
-  isComplete (_, rest) = rest == ""
-  in fst <$> find isComplete (readP_to_S instruction s)
-
-parse :: String -> [Instruction]
-parse = mapMaybe parseInstruction . lines
-
--- Actual calculation on the matrix
 
 gridWidth, gridHeight :: Int
 gridWidth = 1000
---
+++ src/AoC2015/Day06/Types.hs
@@ -0,0 +1,8 @@
+module AoC2015.Day06.Types where
+
+data Instruction = Instruction Op Range deriving Show
+data Op = Off | On | Toggle | Nop deriving Show
+data Range = Range Start End deriving Show
+type Start = Point
+type End = Start
+data Point = Point Int Int deriving Show
