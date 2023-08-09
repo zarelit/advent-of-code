5a51260af666885963069a5537d9f8c5c367877a
David <david@zarel.net>
2023-08-09 20:22:26 +0200

    Add haskell support

--- devenv.nix
+++ devenv.nix
@@ -1,28 +1,5 @@
-{ pkgs, ... }:
-
 {
-  # https://devenv.sh/basics/
-  env.GREET = "devenv";
-
-  # https://devenv.sh/packages/
-  packages = [ pkgs.git ];
-
-  # https://devenv.sh/scripts/
-  scripts.hello.exec = "echo hello from $GREET";
-
-  enterShell = ''
-    hello
-    git --version
-  '';
-
-  # https://devenv.sh/languages/
-  # languages.nix.enable = true;
-
-  # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
-
-  # https://devenv.sh/processes/
-  # processes.ping.exec = "ping example.com";
-
-  # See full reference at https://devenv.sh/reference/options/
+  languages.haskell.enable = true;
+  pre-commit.hooks.fourmolu.enable = true;
+  pre-commit.hooks.hlint.enable = true;
 }
